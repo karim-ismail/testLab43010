0b3c3f9a85927f1568a41d021a8a7466cc680f01
karimismail3 <karimismail3@mc6045-34.labs.sce.carleton.ca>
2018-10-02 09:10:30 -0400

    fixed typo

--- lab4git/src/lab4git/Hello.java
+++ lab4git/src/lab4git/Hello.java
@@ -2,7 +2,7 @@ package lab4git;
 
 public class Hello {
 
-	public static main(String[] args) 
+	public static void main(String[] args) 
 	{
 		Hello hello = new Hello();
 		hello.methodA();
@@ -12,7 +12,7 @@ public class Hello {
 
 	public void methodA(void ) { }
 
-	public void methodb(void) { }
+	public void methodB(void) { }
 
 	public void methodC(void) { }
 }
