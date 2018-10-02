3305791e5141cde9438a51e0f77ce343595c0f92
karimismail3 <karimismail3@mc6045-34.labs.sce.carleton.ca>
2018-10-02 09:07:53 -0400

    added hello.java

---
+++ lab4git/src/lab4git/Hello.java
@@ -0,0 +1,18 @@
+package lab4git;
+
+public class Hello {
+
+	public static main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA(void ) { }
+
+	public void methodb(void) { }
+
+	public void methodC(void) { }
+}
